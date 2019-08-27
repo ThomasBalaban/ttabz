8d923dc5bb1ff89b649b3d4b3687695f6d8adfce
ThomasBalaban <balabaninteractive@gmail.com>
2019-08-26 22:32:01 -0400

    simple navigation animated

--- src/app/template/nav/nav.component.ts
+++ src/app/template/nav/nav.component.ts
@@ -8,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 
 export class NavComponent implements OnInit {
 
+  active: false;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  openNav() {
+    this.active = !this.active;
   }
 
 }

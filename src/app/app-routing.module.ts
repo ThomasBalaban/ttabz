934465177f6daa0323489478ade502f9ffce7ce7
ThomasBalaban <balabaninteractive@gmail.com>
2019-08-28 16:41:56 -0400

    routing to all pages added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PortfolioPageComponent } from './static/portfolio-page/portfolio-page.component';
+import { HomePageComponent } from './static/home-page/home-page.component';
+import { AboutUsPageComponent } from './static/about-us-page/about-us-page.component';
+import { ContactPageComponent } from './static/contact-page/contact-page.component';
+import { ServicesPageComponent } from './static/services-page/services-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'portfolio', component: PortfolioPageComponent },
+  { path: 'about', component: AboutUsPageComponent },
+  { path: 'contact', component: ContactPageComponent },
+  { path: 'services', component: ServicesPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
